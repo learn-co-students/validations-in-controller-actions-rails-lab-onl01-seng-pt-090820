bdfc3eb192080d8bcf8d7b0ff151265f8d03b5ab
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-01-22 19:32:56 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validate :correct_category
+    validates :content, length: { minimum: 100 }
+
+    def correct_category
+        if self.category != "Fiction" && self.category != "Non-Fiction"
+            errors.add(:category, "The category must be fiction or non-fiction.")
+        end
+    end
 end
