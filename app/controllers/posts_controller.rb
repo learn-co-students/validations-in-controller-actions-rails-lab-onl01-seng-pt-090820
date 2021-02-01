4d46216c1de1afb8864793cc2cd7f452e6c2fbb6
Alice Genkin <agenkin1@gmail.com>
2021-02-01 09:41:20 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,15 +5,16 @@ class PostsController < ApplicationController
   end
 
   def edit
-  end
+  end 
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
-  private
 
   def post_params
     params.permit(:category, :content, :title)
@@ -22,4 +23,5 @@ class PostsController < ApplicationController
   def set_post!
     @post = Post.find(params[:id])
   end
+
 end
